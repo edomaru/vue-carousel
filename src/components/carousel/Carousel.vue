24cbdc554b7f6c4166954e6ac6c190c2b90e958c
Eding Muhamad <masaruedo@yahoo.com>
2021-05-24 12:37:07 +0700

    add slide indicators

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="carousel">
     <div class="carousel-inner">
+      <carousel-indicators
+        :total="slides.length"
+        :current-index="currentSlide"
+        @switch="switchSlide($event)"
+      ></carousel-indicators>
       <carousel-item
         v-for="(slide, index) in slides"
         :slide="slide"
@@ -19,10 +24,11 @@
 <script>
 import CarouselItem from "./CarouselItem.vue";
 import CarouselControls from "./CarouselControls.vue";
+import CarouselIndicators from "./CarouselIndicators.vue";
 
 export default {
   props: ["slides"],
-  components: { CarouselItem, CarouselControls },
+  components: { CarouselItem, CarouselControls, CarouselIndicators },
   data: () => ({
     currentSlide: 0,
     slideInterval: null,
@@ -32,21 +38,25 @@ export default {
     setCurrentSlide(index) {
       this.currentSlide = index;
     },
-    prev() {
+    prev(step = -1) {
       const index =
-        this.currentSlide > 0 ? this.currentSlide - 1 : this.slides.length - 1;
+        this.currentSlide > 0
+          ? this.currentSlide + step
+          : this.slides.length - 1;
       this.setCurrentSlide(index);
       this.direction = "left";
       this.startSlideTimer();
     },
-    _next() {
+    _next(step = 1) {
       const index =
-        this.currentSlide < this.slides.length - 1 ? this.currentSlide + 1 : 0;
+        this.currentSlide < this.slides.length - 1
+          ? this.currentSlide + step
+          : 0;
       this.setCurrentSlide(index);
       this.direction = "right";
     },
-    next() {
-      this._next();
+    next(step = 1) {
+      this._next(step);
       this.startSlideTimer();
     },
     startSlideTimer() {
@@ -57,6 +67,14 @@ export default {
     },
     stopSlideTimer() {
       clearInterval(this.slideInterval);
+    },
+    switchSlide(index) {
+      const step = index - this.currentSlide;
+      if (step > 0) {
+        this.next(step);
+      } else {
+        this.prev(step);
+      }
     },
   },
   mounted() {
---
+++ src/components/carousel/CarouselIndicators.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="carousel-indicators">
+    <button
+      class="carousel-indicator-item"
+      :class="{ active: currentIndex === index }"
+      v-for="(item, index) in total"
+      :key="index"
+      @click="$emit('switch', index)"
+    ></button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["switch"],
+  props: ["total", "currentIndex"],
+};
+</script>
+
+<style scoped>
+.carousel-indicators {
+  position: absolute;
+  transform: translateX(-50%);
+  left: 50%;
+  bottom: 1.5em;
+  z-index: 2;
+}
+.carousel-indicator-item {
+  width: 15px;
+  height: 15px;
+  border: none;
+  background: #fff;
+  opacity: 0.5;
+  margin: 0.2em;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.active {
+  opacity: 1;
+}
+</style>
