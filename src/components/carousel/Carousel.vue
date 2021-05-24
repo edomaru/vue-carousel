286b2344f8b9b3bdfa2cb1ae06b6f60827730a4d
Eding Muhamad <masaruedo@yahoo.com>
2021-05-24 11:18:47 +0700

    add basic styling

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -18,3 +18,16 @@ export default {
   components: { CarouselItem },
 };
 </script>
+
+<style scoped>
+.carousel {
+  display: flex;
+  justify-content: center;
+}
+.carousel-inner {
+  position: relative;
+  width: 900px;
+  height: 400px;
+  overflow: hidden;
+}
+</style>
--- src/components/carousel/CarouselItem.vue
+++ src/components/carousel/CarouselItem.vue
@@ -9,3 +9,13 @@ export default {
   props: ["slide"],
 };
 </script>
+
+<style scoped>
+.carousel-item {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
