9b7b0fffba890660d7efbc68d541633f43bb7ba6
Eding Muhamad <masaruedo@yahoo.com>
2021-05-24 11:32:03 +0700

    Adding transition

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -5,6 +5,8 @@
         v-for="(slide, index) in slides"
         :slide="slide"
         :key="`item-${index}`"
+        :current-slide="currentSlide"
+        :index="index"
       ></carousel-item>
     </div>
   </div>
@@ -16,6 +18,25 @@ import CarouselItem from "./CarouselItem.vue";
 export default {
   props: ["slides"],
   components: { CarouselItem },
+  data: () => ({
+    currentSlide: 0,
+    slideInterval: null,
+  }),
+  methods: {
+    setCurrentSlide(index) {
+      this.currentSlide = index;
+    },
+  },
+  mounted() {
+    this.slideInterval = setInterval(() => {
+      const index =
+        this.currentSlide < this.slides.length - 1 ? this.currentSlide + 1 : 0;
+      this.setCurrentSlide(index);
+    }, 3000);
+  },
+  beforeUnmount() {
+    clearInterval(this.slideInterval);
+  },
 };
 </script>
 
--- src/components/carousel/CarouselItem.vue
+++ src/components/carousel/CarouselItem.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="carousel-item">
-    <img :src="slide" />
-  </div>
+  <transition name="slide-in">
+    <div class="carousel-item" v-show="currentSlide === index">
+      <img :src="slide" />
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  props: ["slide"],
+  props: ["slide", "currentSlide", "index"],
 };
 </script>
 
@@ -17,5 +19,15 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
+}
+.slide-in-enter-active,
+.slide-in-leave-active {
+  transition: all 1s ease-in-out;
+}
+.slide-in-enter-from {
+  transform: translateX(-100%);
+}
+.slide-in-leve-to {
+  transform: translateX(100%);
 }
 </style>
